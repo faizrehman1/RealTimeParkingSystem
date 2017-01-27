b427889315fb51a3d426cade7bd73afdf50a5135
faizrehman1 <faizkhanabid@yahoo.com>
2017-01-27 12:30:17 -0800

    add online Parking System in github

---
+++ app/src/main/java/com/example/faizrehman/online_parking_system/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.faizrehman.online_parking_system;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "MainThread";
+    private EditText email,pass;
+    private Button loginbtn;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference firebase;
+    private   TextView signupText;
+    private android.support.v4.app.FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        firebase = FirebaseDatabase.getInstance().getReference();
+        email = (EditText)findViewById(R.id.editText_Loginemail);
+        pass = (EditText)findViewById(R.id.editText_loginpass);
+        loginbtn = (Button)findViewById(R.id.login_btn);
+        signupText = (TextView)findViewById(R.id.sign_up);
+        fragmentManager = getSupportFragmentManager();
+
+
+        signupText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction().add(R.id.container, new signup_fragment()).addToBackStack(null).commit();
+            }
+        });
+
+
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emails = email.getText().toString();
+                String passo = pass.getText().toString();
+
+
+
+                if (emails.length() == 0) {
+                    email.setError("This is Required Field");
+                } else if (passo.length() == 0 && passo.length() <= 6) {
+                    pass.setError("This is Required Field");
+                }
+
+                if (email.getText().toString().matches("admin") && pass.getText().toString().matches("admin")) {
+                    Intent intent = new Intent(MainActivity.this, AdminActivity.class);
+                    startActivity(intent);
+
+                    Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                } else {
+                    try {
+                        final ProgressDialog progressDialog = ProgressDialog.show(MainActivity.this, "Sign In", "Connecting...", true, false);
+
+                        mAuth.signInWithEmailAndPassword(emails, passo).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull final Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    AppLogs.logd("signInWithEmail:onComplete:" + task.isSuccessful());
+                                    progressDialog.dismiss();
+                                  //  Intent intent = new Intent(MainActivity.this, AdminActivity.class);
+                                  //  startActivity(intent);
+                                } else if (!task.isSuccessful()) {
+                                    AppLogs.logw("signInWithEmail" + task.getException());
+                                    Toast.makeText(MainActivity.this, "" + task.getException(),
+                                            Toast.LENGTH_LONG).show();
+                                    progressDialog.dismiss();
+
+                                }
+                            }
+                        }).addOnFailureListener(MainActivity.this, new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                AppLogs.d("FailureSignin", e.getMessage());
+                            }
+                        });
+
+
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/faizrehman/online_parking_system/UserModels.java
@@ -0,0 +1,74 @@
+package com.example.faizrehman.online_parking_system;
+
+/**
+ * Created by faizrehman on 1/27/17.
+ */
+
+public class UserModels {
+    private String email;
+    private String password;
+    private String cpassword;
+    private String userID;
+    private String fname;
+    private String lname;
+
+    public UserModels() {
+    }
+
+    public UserModels(String email, String password, String cpassword, String userID, String fname, String lname) {
+        this.email = email;
+        this.password = password;
+        this.cpassword = cpassword;
+        this.userID = userID;
+        this.fname = fname;
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCpassword() {
+        return cpassword;
+    }
+
+    public void setCpassword(String cpassword) {
+        this.cpassword = cpassword;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+}
